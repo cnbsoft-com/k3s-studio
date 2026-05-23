ab1b4f88d46d0429938aa1dac67dbbca122caa27
dino <dino@cnbsoft.com>
2026-05-23 18:56:36 +0900

    chore: migrate build.gradle to build.gradle.kts

---
+++ apps/mpk3s-api/build.gradle.kts
@@ -0,0 +1,40 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+plugins {
+    java
+    id("org.springframework.boot") version "3.4.4"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "io.mpk3s"
+version = "0.1.0"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("com.fasterxml.jackson.module:jackson-module-parameter-names")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("com.hierynomus:sshj:0.38.0")
+
+    runtimeOnly("org.postgresql:postgresql")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
